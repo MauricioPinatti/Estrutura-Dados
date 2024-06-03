99a5eceb7aae8eed78a11845727bffd9f9f70fa8
Maurício Luiz Pinatti <mauricio.pinatti@gmail.com>
2024-06-03 13:48:43 -0300

    feat: Insertion Sort

---
+++ src/insertionsort/Main.java
@@ -0,0 +1,38 @@
+package insertionsort;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] vetor = new int[10];
+
+        System.out.println("Desordenado");
+        // preenche o vetor com números aleatórios
+        for(int i = 0; i < vetor.length; i++){
+            vetor[i] = (int) (Math.random() * vetor.length);
+            System.out.println(vetor[i]);
+        }
+
+        long inicio = System.currentTimeMillis();
+        long fim;
+        /*
+            INSERTION SORT - Complexidade O(N^2) quadrado
+            i não precisa iniciar no 0, pois vamos testar o segundo com o primeiro
+            sempre testamos com as posições anteriores do vetor  comparando com o valor em aux
+
+        */
+        int aux, j;
+        for(int i = 1; i < vetor.length; i++){
+            aux = vetor[i];
+            j = i -1;
+            while (j >= 0 && vetor[j] > aux){
+                vetor[j+1] = vetor[j];
+                j--;
+            }
+            vetor[j+1] = aux;
+        }
+
+        System.out.println("\n\nOrdenado");
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println(vetor[i]);
+        }
+    }
+}
