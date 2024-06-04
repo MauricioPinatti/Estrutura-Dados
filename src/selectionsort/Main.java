73ab27cea83c02aa9c5f680f6dfbb0e0ed936544
Maurício Luiz Pinatti <mauricio.pinatti@gmail.com>
2024-06-04 11:42:32 -0300

    feat: Selection Sort

--- src/selectionsort/Main.java
+++ src/selectionsort/Main.java
@@ -3,9 +3,13 @@ package selectionsort;
 public class Main {
 
     /*
-        SELECTION SORT - Complexidade O(N^2) quadrado
-        i não precisa iniciar no 0, pois vamos testar o segundo com o primeiro
-        sempre testamos com as posições anteriores do vetor  comparando com o valor em aux
+        SELECTION SORT - Complexidade O(N^2) quadrado (+ rapido que bubble sort, mas quadrática tbm)
+    Verifica qual é o menor elemento da lista, comparando a posição menor que é [i] com a posição [j]
+    guarda a posição menor na variavel aux
+    pega o valor da posição menor e coloca na posição [i](que é a posição que precisa ser ordenada/substituída)
+    e depois colocar o valor da aux, que é a menor posição no vetor da posição [i]
+    ele realiza apenas uma troca, diferente do bublle sort, pois ele só guarda o valor na aux e realiza a troca depois
+
     */
 
     public static void main(String[] args) {
@@ -15,19 +19,26 @@ public class Main {
         // preenche o vetor com números aleatórios
         for(int i = 0; i < vetor.length; i++){
             vetor[i] = (int) (Math.random() * vetor.length);
+        }
+
+        System.out.println("Desordenado");
+        for(int i = 0; i < vetor.length; i++){
             System.out.println(vetor[i]);
         }
 
 
-        int aux, j;
-        for(int i = 1; i < vetor.length; i++){
-            aux = vetor[i];
-            j = i -1;
-            while (j >= 0 && vetor[j] > aux){
-                vetor[j+1] = vetor[j];
-                j--;
+        // SELECTION SORT
+        int posicao_menor, aux;
+        for(int i = 0; i < vetor.length; i++) {
+            posicao_menor = i;
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[j] < vetor[posicao_menor]) {
+                    posicao_menor = j;
+                }
             }
-            vetor[j+1] = aux;
+            aux = vetor[posicao_menor];
+            vetor[posicao_menor] = vetor[i];
+            vetor[i] = aux;
         }
 
         System.out.println("\n\nOrdenado");
